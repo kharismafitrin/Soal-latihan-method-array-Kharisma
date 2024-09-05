41a9167439c670aa194a2bad2526430b2d7cac89
kharismafitrin <kharismanisa14@gmail.com>
2024-09-05 11:05:06 +0700

    eksplorasi mantap

---
+++ eksplorasiArray.js
@@ -0,0 +1,38 @@
+let angka = [12, 5, 8, 130, 44, 15, 7, 19, 23]
+
+//mencari angka genap
+let genap = angka.filter(el =>
+    el % 2 === 0
+)
+console.log("Genap:", genap)
+
+let genap2 = angka.filter(function (el) {
+    return el % 2 === 0
+})
+console.log("Genap2:", genap)
+
+// mencari angka ganjil
+let ganjil1 = angka.filter(el =>
+    el % 2 !== 0
+)
+console.log("Ganjil1:", ganjil1)
+let ganjil2 = angka.filter(function (el) {
+    el % 2 !== 0
+})
+console.log("Ganjil2:", ganjil2)
+
+// mencari angka pertama yang lebih besar dari 10
+let diatas10 = angka.find(el => el > 10)
+console.log("Angka pertama > 10:", diatas10)
+
+// coba map semua angka dikali 2
+let kaliDua = angka.map(el => el * 2)
+console.log("Hasil Kali 2:", kaliDua)
+
+//coba sort desc
+let angkaDesc = [...angka].sort((a, b) => b - a)
+console.log("Urutan besar ke kecil:", angkaDesc)
+
+//coba reverse
+let reverseAngka = [...angka].reverse()
+console.log("Urutan terbalik:", reverseAngka)
--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const collectionNumber = [1, 2, 3, 4, 5, 6]
  * Manipulasi array sederhana
  * Buat array baru berisi angka2 yg merupakan hasil perkalian 3 dari setiap element array collectionNumber
  */
+let kali3 = collectionNumber.map(el => el * 3)
+console.log(kali3);
 
 
 
@@ -10,3 +12,7 @@ const colors = ["blue", "black", "purple", "white", "pink"]
 /**
  * buat array baru yang isinya element yang hurufnya lebih dari 4 berdasarkan array colors
  */
+let warnaLebihDari4 = colors.filter(function (el) {
+    return el.length > 4
+})
+console.log(warnaLebihDari4)
